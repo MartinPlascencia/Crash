417f53cba8d072af97df947aa7fbbccf04c2bb6d
MartinPlascencia <martin.plascencia@live.com.mx>
2026-09-24 20:34:15 -0600

    Implementado Roll con efectos y empezada moneda.

--- Assets/Scripts/RollController.cs
+++ Assets/Scripts/RollController.cs
@@ -6,15 +6,29 @@ public class RollController : MonoBehaviour
     private InputController inputController;
     [SerializeField]
     private UnityEvent onRoll;
+    [SerializeField]
+    private float rollDuration = 1f;
+    [SerializeField]
+    private GameObject rollEffectPrefab;
+    [SerializeField]
+    private float effectOffsetY = 0.5f;
+    private bool isRolling = false;
     private void Update()
     {
-        if (inputController.Roll)
+        if (!isRolling && inputController.Roll)
         {
             Roll();
         }
     }
     private void Roll()
     {
+        PoolManager.Instance.GetObject(rollEffectPrefab, transform.position + (Vector3.up * effectOffsetY));
+        isRolling = true;
         onRoll?.Invoke();
+        Invoke(nameof(EndRoll), rollDuration);
+    }
+    private void EndRoll()
+    {
+        isRolling = false;
     }
 }
